2e3ea9f52748dab364fc6982759521c0d9fbb2a8
DESKTOP-U0NRHUR\Fr7nk <frolp91@gmail.com>
2021-04-29 13:37:22 -0600

    using npm commands

---
+++ usign-npm/app.js
@@ -0,0 +1,16 @@
+const _ = require("lodash");
+let example = _.fill([1, 2, 3, 4, 5], "banana", 1, 4);
+console.log(example);
+
+/* 
+
+npm init
+// Create a package
+    -name
+    -description
+    -entrypoint : starting file for entry point (command)
+
+// npmjs.com
+npm install {packageName} -> adds to dependecies
+
+*/
