09c3cfcaae1e743aebfaf38dd516e588c0d8a4af
DESKTOP-U0NRHUR\Fr7nk <frolp91@gmail.com>
2021-04-29 12:41:37 -0600

    retrieving static files

--- http-files/app.js
+++ http-files/app.js
@@ -3,11 +3,17 @@ const fs = require("fs");
 
 http
   .createServer((request, response) => {
-    const readStream = fs.createWriteStream("./index.html");
-    // Set a header
-    const OK = 200;
-    response.writeHead(OK, { "Content-type": "text/html" });
-    // Put all the content chunk by chunk into the response object
-    readStream.pipe(response);
+    // retrieveObject("./http-files/index.html", response, "text/html");
+    // retrieveObject("./http-files/example.json", response, "application/json");
+    retrieveObject("./http-files/example.png", response, "image/png");
   })
   .listen(3000);
+
+const retrieveObject = (path, response, type) => {
+  const readStream = fs.createReadStream(path);
+  // Set a header
+  const OK = 200;
+  response.writeHead(OK, { "Content-type": type });
+  // Put all the content chunk by chunk into the response object
+  readStream.pipe(response);
+};
